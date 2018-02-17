114824c13e360bf58b31f68d62dbf31fa8b5be67
Agastya Asthana <agastya.asthana@gmail.com>
2018-02-17 10:59:42 -0600

    Replacing old files in Vision Project

--- Vision/Compile.sh
+++ Vision/Compile.sh
@@ -1,2 +1,6 @@
-g++ $(pkg-config --libs --cflags opencv) -o VisionProcessing VisionProcessing.cpp -lraspicam -lraspicam_cv -lopencv_core
-
+g++ $(pkg-config --libs --cflags opencv) -o VisionProcessing -std=c++11 -L/home/pi/ntcore-4.0.0/build/libs/ntcore/shared -L/home/pi/wpiutil/build/libs/wpiutil/shared -lraspicam -lraspicam_cv -lopencv_core -lntcore -lwpiutil -I/home/pi/wpiutil/src/main/native/include -I/home/pi/ntcore-4.0.0/src/main/native/include -I/home/pi/raspicam-0.1.6/src VisionProcessing.cpp
+#sudo /sbin/ldconfig -v
+#export ntcore-4.0.0/build/libs/ntcore/shared
+#$LD_LIBRARY_PATH = /usr/local/lib
+#LD_LIBRARY_PATH = $LD_LIBRARY_PATH:/shared/
+#export LD_LIBRARY_PATH
--- Vision/VisionProcessing.cpp
+++ Vision/VisionProcessing.cpp
@@ -3,7 +3,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <raspicam.h>
 #include <raspicam_cv.h>
-//#include <networktables/NetworkTable.h>
 #include <networktables/NetworkTable.h>
 #include <networktables/NetworkTableInstance.h>
 #include <iostream>
@@ -28,22 +27,34 @@ using namespace nt;
 RaspiCam_Cv Camera;
 vector<Vec4i> hierarchy;
 vector<vector<Point> > contours;
-shared_ptr<NetworkTable> netTable; 	// X,Y,Z is the order for the coordinates
-//NetworkTableInstance *netTableInstance;
 Mat image;
 Mat inrange;
 Mat drawing;
+Mat PowerCube = imread("PowerCube.jpg", CV_LOAD_IMAGE_COLOR);
 NetworkTableInstance nt_Inst;
-double counter = 0;
-//NetworkTable testNetTable();
-//NetworkTable nTable;
+shared_ptr<NetworkTable> netTable; 	// X,Y,Z is the order for the coordinates
+int counter = 0;
+int index1, index2;
+double im_min_dist1, im_min_dist2, im_actual_dist;
+
+
+
 //VideoCapture cap(0);
-Mat frame;
+//Mat frame;
+
 //Mat imageBrightness;
+
+// Following settings is for camera calibrated value
+const double default_FOV_row_num = 960;
+const double standard_height_p = 400/default_FOV_row_num;
+const double pixel_per_in = 42/default_FOV_row_num;
+const double pixel_per_degree = 10/default_FOV_row_num;
+const double cal_distance_in_inch = 12;
 
 int main()
 {
-	nt_Inst = NetworkTableInstance::Create();
+
+	nt_Inst = NetworkTableInstance::GetDefault();
 	Camera.set(CV_CAP_PROP_GAIN, 1);
 	Camera.set(CV_CAP_PROP_EXPOSURE, 124);
 	//Camera.set(
@@ -57,33 +68,43 @@ int main()
 	}*/
 	//Camera.retrieve(image);
 
-	nt_Inst.StartClient();
+	//nt_Inst.StartClient();
+	nt_Inst.StartClientTeam(1259);
 	//nt_Inst.SetServer("10.12.59.2");
-	nt_Inst.SetServer("roboRIO-1259-frc.local");
+	//nt_Inst.SetServer("roboRIO-1259-frc.local");
 	netTable = nt_Inst.GetTable("OpenCV");//netTable = nt_Inst.GetTable("OpenCV");
-	//netTable->PutNumber("Counter", counter);
-	//NetworkTable::SetIPAddress("10.12.59.2");
+	netTable->GetEntry("visioncounter").Delete();
+	cout<<netTable->GetEntry("visioncounter").Exists()<<endl;
+	netTable->GetEntry("visioncounter").ClearPersistent();
 	//cap>>frame; //takes a frame
 	//cvtColor(frame, image, COLOR_GRAY2RGB;
 	//image.convertTo(imageBrightness, -1, 1, 100);
 
 		//Fix later
-		const Scalar lower = Scalar(0, 65, 65);
-		const Scalar upper = Scalar(5, 115, 115);
+		const Scalar lower = Scalar(15, 90, 90);
+		const Scalar upper = Scalar(70, 250, 250);
 
 		int biggestContour = 0;
 		int biggestContourLocation = 0;
 		int currentContourSize = 0;
 
-	while (true)
-	{
+                double cube_height;    // cube height on image 
+		double dx, dy, dz;     // distance in X, Y, Z between camera and cube
+
+	//while (true)
+	//{
 
 		Camera.grab();
-		counter++;
+//		if(counter%2==0)
+			counter +=3;
+		//else
+		//	counter -=5;
 		Camera.retrieve(image);
 
-		inRange(image, lower, upper, inrange);
+		//inRange(image, lower, upper, inrange);
+		inRange(PowerCube, lower, upper, inrange);
 		findContours(inrange, contours, hierarchy, CV_RETR_LIST,CV_CHAIN_APPROX_NONE);
+
 		drawing = Mat::zeros(inrange.size(), CV_8UC3);
 
 		biggestContour = 0;
@@ -98,19 +119,73 @@ int main()
 				biggestContourLocation = i;
 			}
 		}
-		drawContours(drawing, contours,biggestContourLocation , Scalar(124, 252, 0), 2, 8,hierarchy, 0);
-		Moments m = moments(contours[biggestContourLocation], true);
-		circle(drawing, Point(m.m10/m.m00,m.m01/m.m00), 8, Scalar(255,105,180), 1, LINE_8, 0);
+                             
 
-		netTable->PutNumber("Counter", counter);
+        drawContours(drawing, contours,biggestContourLocation , Scalar(124, 252, 0), 2, 8,hierarchy, 0);
+
+        Moments m = moments(contours[biggestContourLocation], true);
+        circle(drawing, Point(m.m10/m.m00,m.m01/m.m00), 8, Scalar(255,105,180), 1, LINE_8, 0);
+        double cube_center_x = m.m10/m.m00;
+        double cube_center_y = m.m01/m.m00;
+
+        im_min_dist1 = 10000;   // assign a big value
+        im_min_dist2 = 10000;   // assign a big value
+
+
+        int contour_id = biggestContourLocation;
+        /*for (int i = 0; i < contours[contour_id].size(); i++)
+        {
+        	//Point coordinate_of_contour = StartingContour.size();
+        	//cout<<"Contour Position at 1x: "<<coordinate_of_contour.x<<" Contour y is: "<<coordinate_of_contour.y<<endl;
+        	cout<<"Second Contour is at: " <<contours[contour_id][i] <<endl;
+        }*/
+           // cout<<"Size of Contours = " <<biggestContourLocation <<endl;
+
+        // Add calculation for cube_hegith, distance (dx,, dy, dz) from camera to the cube
+        for (int i = 0; i < contours.size(); i++)
+        {
+                im_actual_dist = abs(cube_center_x - contours[biggestContourLocation][i].x);
+                if (im_min_dist1 > im_actual_dist)
+                {
+                        if (cube_center_y < contours[biggestContourLocation][i].y)
+                        {
+                                index1 = i;
+                                im_min_dist1 = im_actual_dist;
+                        }
+                }
+                if (im_min_dist2 > im_actual_dist)
+                {
+                        if (cube_center_y > contours[biggestContourLocation][i].y)
+                        {
+                                index2 = i;
+                                im_min_dist2 = im_actual_dist;
+                        }
+                }
+        }
+        cube_height = contours[biggestContourLocation][index1].y - contours[biggestContourLocation][index2].y;
+
+
+
+		netTable->GetEntry("visioncounter").ForceSetDouble(counter);
+		double roboCounter = netTable->GetEntry("RoboCounter").GetDouble(0);
+		netTable->PutNumber("visioncounter", counter);
 		//netTable->PutNumber("NetworkTableFirstValue", counter+42);
 
-		cout<<"Counter is at: "<<counter<<endl;
-		//imwrite("inrange.bmp",inrange);
-		//imwrite("drawing.bmp", drawing);
-		//imwrite("image.bmp", image);
-	}
+		//cout<<"Counter is at: "<<counter<<endl;
+		//cout<<"RoboRIO is at: "<<roboCounter<<endl;
+		imwrite("inrange.bmp",inrange);
+		imwrite("drawing.bmp", drawing);
+		imwrite("PowerCube.bmp", PowerCube);
+		cout<<"x: "<<m.m10<<" y: "<<m.m01<<" divided by: "<<m.m00<<endl;
+		cout<<"center of x is at: "<<m.m10/m.m00<<" center of y is at: "<<m.m01/m.m00<<endl;
+		cout<<"Index1 is: "<<index1<<" Index2 is: "<<index2<<endl;
+		cout<<"Height: "<<cube_height<<endl;
+	//}
 	Camera.release();
 	return 0;
+
+
+
+
 
 }
